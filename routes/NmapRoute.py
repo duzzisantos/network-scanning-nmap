eaaf3e2d3237489df5d32b659a2d88d1a5cdd0fa
Duzie <duzzi4x@gmail.com>
2025-02-08 16:30:30 +0100

    Added route for providing port states

--- routes/NmapRoute.py
+++ routes/NmapRoute.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 from models.NmapModel import NmapResultModel, ScanArgs
 from api.NmapScan import nmap_port_scan
+from api.NmapPortStates import nmap_port_states
 
 nmap_scanning_router = APIRouter()
 
 
-@nmap_scanning_router.post("/GetPortScanningResults")
-def port_scanning():
-    return nmap_port_scan(ScanArgs)
+@nmap_scanning_router.post("/GetPortScanningDetails")
+def port_scanning(model: ScanArgs):
+    return nmap_port_scan(model)
+
+
+@nmap_scanning_router.post("/GetPortScanningState")
+def port_state(model: ScanArgs):
+    return nmap_port_states(model)
